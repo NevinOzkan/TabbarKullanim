2bfbce94557eebb9a82e79647e1ff7d3f08e1fcf
NevinOzkan <ozkannevin73@gmail.com>
2023-02-27 17:12:30 +0300

    Initial Commit

---
+++ Tabbar_kullanım/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tabbar_kullanım
+//
+//  Created by Nevin Özkan on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
